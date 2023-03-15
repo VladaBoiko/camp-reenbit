a81ae9514871cebd784e1708ddccc7d3546fb1d5
VladaBoiko <102071674+VladaBoiko@users.noreply.github.com>
2023-03-15 23:03:11 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import { lazy } from "react";
-import { Layout } from "./components/Layout";
-const MainPage = lazy(() => import("./pages/MainPage/MainPage"));
-const CardDetail = lazy(() => import("./pages/CardDetails/CardDetails"));
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { lazy } from 'react';
+import { Layout } from './components/Layout';
+const MainPage = lazy(() => import('./pages/MainPage/MainPage'));
+const CardDetail = lazy(() => import('./pages/CardDetails/CardDetails'));
 export const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<MainPage />} />
-          <Route path="/:id" element={<CardDetail />} />
-
+          <Route index element={<MainPage />}>
+            <Route path="/:id" element={<CardDetail />} />
+          </Route>
           <Route path="*" element={<Navigate to="/" />} />
         </Route>
       </Routes>
