3e960e8b39ece543664410f99c163232695bf9c8
VladaBoiko <102071674+VladaBoiko@users.noreply.github.com>
2023-03-15 22:41:02 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/camp-reenbit/">
+        <BrowserRouter basename="/camp-reenbit">
           <App />
         </BrowserRouter>
       </PersistGate>
